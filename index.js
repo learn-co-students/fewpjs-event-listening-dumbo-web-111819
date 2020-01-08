5f443be5e248bd46403a5f78af798cf726d3fc5e
ajshemi <akpojotor@yahoo.com>
2020-01-07 20:02:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+    console.log('I was clicked!');
+});
 }
+//the function addingEventListener() has to be called in the console for it to work.
+//the block without the function will run on the console automatically. 
